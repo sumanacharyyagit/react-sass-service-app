fe2c76752bb9faf0f40731a6f34d2b53f700e544
sumanacharyyagit <sumanacharyya999@gmail.com>
2023-03-19 03:22:08 +0530

    Completed application

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Suman</h1>
+        <p>&copy; all rights reserved.</p>
+      </div>
+      <div>
+        <h5>Follow Me</h5>
+        <div>
+          <a href="https://suman-dev.in" rel="noreferrer" target={"_blank"}>
+            Portfolio
+          </a>
+          <a href="https://github.com" rel="noreferrer" target={"_blank"}>
+            Github
+          </a>
+          <a href="https://instagram.com" rel="noreferrer" target={"_blank"}>
+            Instagram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
